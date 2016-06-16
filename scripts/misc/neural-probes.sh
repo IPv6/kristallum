1103d07ed902c9c9f8287910dd5044504fb24291
IPv6 <ipv6@Nikolays-iMac.local>
2016-06-16 17:14:58 +0300

    ext problem

--- scripts/misc/neural-probes.sh
+++ scripts/misc/neural-probes.sh
@@ -1,4 +1,4 @@
 # https://github.com/jcjohnson/neural-style
 cd ~/Documents/Zzz/neurpaint/neural-style-master/
-~/torch/install/bin/th neural_style.lua -style_image ../style.jpg -content_image ../target.jpg -output_image ../result -init image -content_weight 100 -style_weight 200 -normalize_gradients -gpu -1 -num_iterations 1000
+~/torch/install/bin/th neural_style.lua -style_image ../style.jpg -content_image ../target.jpg -output_image ../result.jpg -init image -content_weight 100 -style_weight 200 -normalize_gradients -gpu -1 -num_iterations 1000
 #-init image -content_weight 100 -style_weight 1500 -normalize_gradients
