dd4c4d189881133b8d15120056ea4f3255a78895
IPv6 <ipv6@Nikolays-iMac.local>
2016-09-02 13:51:13 +0300

    moving stuff

--- scripts/misc/neural-probes.sh
+++
@@ -1,4 +0,0 @@
-# https://github.com/jcjohnson/neural-style
-cd ~/Documents/Zzz/neurpaint/neural-style-master/
-~/torch/install/bin/th neural_style.lua -style_image ../style.jpg -content_image ../target.jpg -output_image ../result.jpg -init image -content_weight 100 -style_weight 200 -normalize_gradients -gpu -1 -num_iterations 1000
-#-init image -content_weight 100 -style_weight 1500 -normalize_gradients
